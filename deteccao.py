aed2f95888047bc1fbbf934844f56dd8b9851007
Fazenda-png <fazenda.j.p.s@hotmail.com>
2022-10-31 21:45:30 -0300

    terminou

--- deteccao.py
+++ deteccao.py
@@ -1,7 +1,20 @@
 import cv2 as cv
 
-camera = cv.VideoCapture(0, cv.CAP_DSHOW)
-
+camera = cv.VideoCapture("cats.mp4")
 cascade = cv.CascadeClassifier("treinamento/cascade.xml")
-
 while True:
+    
+    _, imagem = camera.read()
+    gray = cv.cvtColor(imagem, cv.COLOR_BGR2GRAY)
+    objetos = cascade.detectMultiScale(gray, 1.25, 5)
+
+    for (x,y,w,h) in objetos:
+        cv.rectangle(imagem, (x,y),(x+w,y+h),(0,0,255),2)
+
+    cv.imshow("Gato", imagem)
+    k = cv.waitKey(60)
+    if k == 27:
+        break
+    
+cv.destroyAllWindows()
+camera.release()
--- negative/negative.py
+++ negative/negative.py
@@ -1,9 +1,7 @@
 import os
 
-for file_type in ['negatives']:
+for file_type in ['negative/imagens']:
     for img in os.listdir(file_type):
         line = file_type+"/"+img+"\n"
         with open("negatives.txt","a") as f:
             f.write(line)
-
-
